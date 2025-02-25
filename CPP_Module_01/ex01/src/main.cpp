7c82f9bd462ecff51a92f9cd7a0a5c97ff663f63
samreerink <samreerink@gmail.com>
2025-02-25 22:48:03 +0100

    CPP01/ex01 done, implemented error check

--- CPP_Module_01/ex01/src/main.cpp
+++ CPP_Module_01/ex01/src/main.cpp
@@ -1,11 +1,10 @@
 #include "../includes/Zombie.hpp"
-#include <stdlib.h>
 
 int	main() {
-
-	Zombie* Horde;
 	int n_zombies = 5;
-	Horde = zombieHorde(n_zombies, "Sam");
+	Zombie* Horde = zombieHorde(n_zombies, "Sam");
+	if (!Horde)
+		return 1;
 	for (int i = 0; i < n_zombies; i++) {
 		Horde[i].announce();
 	}
--- CPP_Module_01/ex01/src/zombieHorde.cpp
+++ CPP_Module_01/ex01/src/zombieHorde.cpp
@@ -1,10 +1,15 @@
 #include "../includes/Zombie.hpp"
+#include <iostream>
 
 Zombie* zombieHorde(int N, std::string name) {
-	Zombie* Horde;
-	Horde = new Zombie[N];
-	for (int i = 0; i < N; i++) {
-		Horde[i].setZombieName(name);
+	try {
+		Zombie* Horde = new Zombie[N];
+		for (int i = 0; i < N; i++) {
+			Horde[i].setZombieName(name);
+		}
+		return Horde;
+	} catch (const std::bad_alloc& e) {
+		std::cerr << "Memory allocation failed: " << e.what() << std::endl;
+		return nullptr;
 	}
-	return Horde;
 }
