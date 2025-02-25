f72521459af0c04461103622cd5bcc8c90ce5080
samreerink <samreerink@gmail.com>
2025-02-25 23:15:36 +0100

    Module01/ex02 done

---
+++ CPP_Module_01/ex02/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+	std::cout << "Memory address of string variable  : " << &str << "\n";
+	std::cout << "Memory address held by stringPTR   : " << stringPTR << "\n";
+	std::cout << "Memory address held by stringREF   : " << &stringREF << "\n";
+	std::cout << "Value of string variable           : " << str << "\n";
+	std::cout << "Value pointed to by stringPTR      : " << *stringPTR << "\n";
+	std::cout << "Value pointed to by stringREF      : " << stringREF << "\n";
+	return 0;
+}
